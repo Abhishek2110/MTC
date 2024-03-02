3c2d2e6f2297e73142ab7acee2d9dba4b27ddd27
Abhishek Rajpal <86553778+Abhishek2110@users.noreply.github.com>
2024-03-03 00:07:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ with st.sidebar:
         st.title("Mangifera Indentika")
         st.subheader("Accurately indentifies mango breeds from the uploaded images")          
 
-# @st.cache(allow_output_mutation=True)
+@st.cache(allow_output_mutation=True)
 def load_model():
     model=tf.keras.models.load_model('EfficientNetB2-mangoes-100.0.h5')
     return model
